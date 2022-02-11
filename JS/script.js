53ec4eba41e444918d2d0bddd638ad6c9fb058ac
MixterOne <matheusisaias879@gmail.com>
2022-02-11 20:17:08 -0300

    confirmar

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,92 @@
 /*FUNÇÃO DE CONVERSÃO DO APORTE INICIAL*/
 
-let realLeft = document.getElementById('realLeft');
+let realLeft = document.getElementById('realLeft').value;
 
 function convertValor (valor) {
 	let valorSemCurrency = valor.replace(/r\$\s?/i, '');
 	return isNaN(valorSemCurrency) ? 0 : parseFloat(valorSemCurrency);
 }
 
-realLeft.addEventListener("blur", function() {
+
+/* 
+  realLeft.addEventListener("blur", function(){
   
   let valor = convertValor(this.value);
   let valor2 = valor.toLocaleString("pt-br", {
 	style: "currency",
 	currency: "BRL"
-  });
-  realLeft.value = valor2;
+});
+
+*/
+
+  /* 
+    CHAT:
+    nem pra ter avisado em
+    tenta agora, tenho fé agora
+   
+    cara ta dando que nn ta definido
+    panErrorLeft.style.color = 'red'
+        aporteErrorLeft.style.color = 'red'
+    não apaga, deixa eu terminar 
+
+  CARA TA CARREGANDO INFINITAMENTE kkkkkkk ACHO QUE É O LOOP
+
+  como assim carregando infinitamente?
+
+  fica rodando a bolinha de carregando a pagina sabe? ae nn me deixar clicar em nd
+  tem nada haver não, só verifica lá nos input, coloca uma string e depois um número
+  se fica vermelho e verde  
+  
+  ae nn me deixar clicar em nd
+  ae nn me deixar clicar em nd
+  ae nn me deixar clicar em nd
+  ae nn me deixar clicar em nd
+
+  nem sei o que vc colocou ali em baixo, du nada começou a escreve sem falar nada, vou tentar terminar ali
+  e vc verifica
+
+  é pq no console ta dizendo que nn tinha sido definido
+  
+  testaspanErrorLeft.style.color = 'red'
+        aporteErrorLeft.style.color = 'red' a
+
+
+        testa a porra do código agora
+        continua carrega
+  */
+
+        
+
+    let n = 2;
+    let newvalue = Number(realLeft);
+    while( n > 1 ){
+      let spanErrorLeft = document.querySelector('.errorLeft');
+      let aporteErrorLeft = document.querySelector('.aporteLeft');
+
+      if (newvalue != Number) {
+        spanErrorLeft.style.color = 'red'
+        aporteErrorLeft.style.color = 'red'
+      }
+      else {
+        spanErrorLeft.style.color = 'green'
+        aporteErrorLeft.style.color = 'green'
+      }
+    }
+
+
 
   /*ERROS NOS INPUTS------- */
 
-  let spanErrorLeft = document.querySelector('.errorLeft');
-  let aporteErrorLeft = document.querySelector('.aporteLeft')
-
-  if(realLeft.value != Number){
-    spanErrorLeft.style.color = 'red'
-    aporteErrorLeft.style.color = 'red'
-    spanErrorLeft.innerHTML = 'Aporte deve ser um número!'
-  }
-});
+  
 
 
+  // if(realLeft != Number){
+  //  spanErrorLeft.style.color = 'red'
+//  aporteErrorLeft.style.color = 'red'
+   // spanErrorLeft.innerHTML = 'Aporte deve ser um número!'
+ // }
 
-
+  
 
 
 /*FUNÇÃO DE CONVERSÃO DO APORTE MENSAL*/
@@ -39,8 +95,8 @@ realLeft.addEventListener("blur", function() {
 let realRight = document.getElementById('realRight');
 
 function convertValor (valor) {
-	let valorSemCurrency2 = valor.replace(/r\$\s?/i, '');
-	return isNaN(valorSemCurrency2) ? 0 : parseFloat(valorSemCurrency2);
+	let valorSemCurrency = valor.replace(/r\$\s?/i, '');
+	return isNaN(valorSemCurrency) ? 0 : parseFloat(valorSemCurrency);
 }
 
 realRight.addEventListener("blur", function() {
@@ -61,6 +117,45 @@ realRight.addEventListener("blur", function() {
     spanErrorRight.style.color = 'red'
     aporteErrorRight.style.color = 'red'
     spanErrorRight.innerHTML = 'Aporte deve ser um número!'
+  }else{
+    spanErrorRight.style.color = 'red'
+    aporteErrorRight.style.color = 'red'
   }
-});
+})
 
+
+
+
+
+
+  
+
+//FUNÇÃO PARA CHEGAR SE OS INPUT ESTÃO PREENCHIDOS E VALIDAR O BUTTON-------
+
+function checkInputs(inputs) {
+  var filled = true;
+  
+  inputs.forEach(function(input) {
+      
+    if(input.value === "") {
+        filled = false;
+    }
+  
+  });
+  
+  return filled;
+  
+}
+var inputs = document.querySelectorAll("input");
+var button = document.querySelector(".simu");
+inputs.forEach(function(input) {
+    
+  input.addEventListener("keyup", function() {
+    if(checkInputs(inputs)) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+      button.style.background = 'rgb(255, 142, 100)'
+    }
+  });
+});
