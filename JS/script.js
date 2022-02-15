6b5c0c1bc1d2b9d0c0bbc0d25e7b9e454ecc4f1d
MixterOne <matheusisaias879@gmail.com>
2022-02-15 17:40:25 -0300

    confirmar

--- JS/script.js
+++ JS/script.js
@@ -1,75 +1,106 @@
-function convertValor (valor) {
-	let valorSemCurrency = valor.replace(/r\$\s?/i, '');
-	return isNaN(valorSemCurrency) ? 0 : parseFloat(valorSemCurrency);
+
+
+
+
+
+function change() {
+  /*ERROO NO APORTE INICIAL------ */
+
+  let realLeft = document.getElementById('realLeft').value;
+  let spanErrorLeft = document.querySelector('.errorLeft');
+  let aporteErrorLeft = document.querySelector('.aporteLeft');
+
+
+  if (isNaN(realLeft) == true || realLeft == "") {
+    spanErrorLeft.style.color = 'red'
+    spanErrorLeft.innerHTML = 'Aporte deve ser um número!'
+    aporteErrorLeft.style.color = 'red'
+  }
+  else if (isNaN(realLeft) == false || realLeft == Number) {
+    spanErrorLeft.style.display = 'none'
+    aporteErrorLeft.style.color = 'rgba(27, 27, 27, 0.836) '
+  }
+
+  //CONVERSÃO DO APORTE INICIAL---
+
+  var atual = document.querySelector('#realLeft');
+
+  function sanitizeValor(valor) {
+    var valorSemCurrency = valor.replace(/r\$\s?/i, '');
+    return isNaN(valorSemCurrency) ? 0 : parseFloat(valorSemCurrency);
+  }
+
+  atual.addEventListener("blur", function () {
+    var valor = sanitizeValor(this.value);
+    var valor2 = valor.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL"
+    });
+    atual.value = valor2;
+  });
+
+
 }
 
+function change2() {
+  //ERROS NO APORTE MENSAL------- 
+  let realRight = document.getElementById('realRight').value;
+  let spanErrorRight = document.querySelector('.errorRight');
+  let aporteErrorRight = document.querySelector('.aporteRight');
 
 
-
-function change () {
-    let realLeft = document.getElementById('realLeft').value;
-      /*ERROS NOS INPUTS------- */
-  
-      let spanErrorLeft = document.querySelector('.errorLeft');
-      let aporteErrorLeft = document.querySelector('.aporteLeft');
-      
-       
-        if (isNaN(realLeft) == true || realLeft == "") {
-          spanErrorLeft.style.color = 'red'
-          spanErrorLeft.innerHTML = 'Aporte deve ser um número!'
-          aporteErrorLeft.style.color = 'red'
-        }
-        else {
-          spanErrorLeft.style.display = 'none'
-          aporteErrorLeft.style.color = 'rgba(27, 27, 27, 0.836) '
-        }
-
-}
-
-function change2(){
-  /*FUNÇÃO DE CONVERSÃO DO APORTE MENSAL*/
-
-let realRight = document.getElementById('realRight').value;
-
-/*ERROS NOS INPUTS------- */
-  
-let spanErrorRight = document.querySelector('.errorRight');
-let aporteErrorRight = document.querySelector('.aporteRight');
-
- 
   if (isNaN(realRight) == true || realRight == "") {
     spanErrorRight.style.color = 'red'
     spanErrorRight.innerHTML = 'Aporte deve ser um número!'
     aporteErrorRight.style.color = 'red'
   }
-  else {
+  else if (isNaN(realRight) == false || realRight == Number) {
     spanErrorRight.style.display = 'none'
     aporteErrorRight.style.color = 'rgba(27, 27, 27, 0.836) '
   }
 }
-        
-//FUNÇÃO PARA CHEGAR SE OS INPUT ESTÃO PREENCHIDOS E VALIDAR O BUTTON-------
+
+//CONVERSÃO DO APORTE MENSAL-----
+var atual = document.querySelector('#realRight');
+
+function sanitizeValor(valor) {
+  var valorSemCurrency = valor.replace(/r\$\s?/i, '');
+  return isNaN(valorSemCurrency) ? 0 : parseFloat(valorSemCurrency);
+}
+
+atual.addEventListener("blur", function () {
+ 
+  var valor = sanitizeValor(this.value);
+  var valor2 = valor.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL"
+  });
+  atual.value = valor2;
+});
+
+//FUNÇÃO PARA CHECAR SE OS INPUTS ESTÃO PREENCHIDOS E VALIDAR O BUTTON-------
 
 function checkInputs(inputs) {
   var filled = true;
-  
-  inputs.forEach(function(input) {
-      
-    if(input.value === "") {
-        filled = false;
+
+  inputs.forEach(function (input) {
+
+    if (input.value === "") {
+      filled = false;
     }
-  
+
   });
-  
+
   return filled;
-  
+
 }
+
 var inputs = document.querySelectorAll("input");
 var button = document.querySelector(".simu");
-inputs.forEach(function(input) {
-    
-  input.addEventListener("keyup", function() {
-    if(checkInputs(inputs)) {
+inputs.forEach(function (input) {
+
+  input.addEventListener("keyup", function () {
+    if (checkInputs(inputs)) {
       button.disabled = false;
     } else {
       button.disabled = true;
