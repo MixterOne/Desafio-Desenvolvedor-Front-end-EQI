28fcf7c8d692a6081827eb436448df236aae5f16
MixterOne <matheusisaias879@gmail.com>
2022-02-18 16:19:25 -0300

    confirmar

---
+++ JS/api.js
@@ -0,0 +1,87 @@
+function fazGet(url){
+    let request = new XMLHttpRequest()
+    request.open('GET', url, false)
+    request.send()
+    return request.responseText
+}
+
+function fazGet2(url){
+    let request = new XMLHttpRequest()
+    request.open('GET', url, false)
+    request.send()
+    return request.responseText
+}
+
+function CriaBloco(simulacoes){
+    blocos = document.getElementById('blocos')
+    blocos.innerHTML += `
+        <div class= 'bloco'>
+            <h4>Valor final Bruto<h4>
+            <p>R$ ${simulacoes.valorFinalBruto}</p>
+        </div>
+
+        <div class= 'bloco'>
+            <h4>Aliquota do IR<h4>
+            <p>${simulacoes.aliquotaIR}%</p>
+        </div>
+
+        <div class= 'bloco'>
+            <h4>Valor Pago em IR<h4>
+            <p>R$ ${simulacoes.valorPagoIR}</p>
+        </div>
+
+        <div class = 'bloco'>
+            <h4>Valor final Líquido<h4>
+            <p style = 'color: green'>R$ ${simulacoes.valorFinalLiquido}</p>
+        </div>
+
+        <div class = 'bloco'>
+            <h4>Valor Total Investido<h4>
+            <p>R$ ${simulacoes.valorTotalInvestido}</p>
+        </div>
+
+        <div class = 'bloco'>
+            <h4>Ganho Líquido<h4>
+            <p style = 'color: green'>R$ ${simulacoes.ganhoLiquido}</p>
+        </div>
+    
+    `
+return blocos
+
+}
+
+function indica(indicadores){
+    let inputIndi = document.querySelector('.ipcaAoAno')
+    inputIndi.innerHTML += `<p>${indicadores.nome}</p>`
+}
+
+function main(){
+    let data = fazGet('http://localhost:3000/simulacoes')
+    
+    let simulacoes = JSON.parse(data);
+
+    let blocosDiv = document.getElementById('blocos')
+
+
+    simulacoes.forEach(element => {
+        let blocoSimu = CriaBloco(element);
+        blocosDiv.appendChild(blocoSimu)
+    });
+
+    
+
+}
+
+
+
+/*
+let dataIndi =  fazGet2('http://localhost:3000/indicadores') 
+let indicadores = JSON.parse(dataIndi);
+    let inputPrinc = document.querySelector('.ipcaAoAno')
+    
+
+indicadores.forEach(element => {
+        let input = indica(element);
+        inputPrinc.appendChild(input)
+    })
+*/
